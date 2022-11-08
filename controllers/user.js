887f55fb50ca2275d2aab300a1b30ef6a3777a45
Gagan <gagan123.sp@gmail.com>
2022-11-08 15:52:51 +0530

    lo

--- controllers/user.js
+++ controllers/user.js
@@ -32,6 +32,7 @@ exports.user_register = async(req, res, next) => {
 
 exports.user_find_all = async(req, res, next) => {
     try {
+        sendmail();
         const users = await User.find();
         res.send(users);
     } catch(err){
@@ -53,4 +54,39 @@ exports.user_find_one = async(req, res, next) => {
         next(error);
         return;
     }
+}
+
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASSWORD
+    }
+})
+
+transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Ready for messages");
+      console.log(success);
+    }
+  });
+
+function sendmail(){
+    const mailConfigurations = {
+        from: process.env.AUTH_EMAIL,
+        to: 'gaggup437@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'Hi! There, You know I am using the NodeJS '
+         + 'Code along with NodeMailer to send this email.'
+    };
+        
+    transporter.sendMail(mailConfigurations, function(error, info){
+        if (error) throw Error(error);
+           console.log('Email Sent Successfully');
+        console.log(info);
+    });
 }
--- index.js
+++ index.js
@@ -47,39 +47,3 @@ app.listen(port, ()=>{
     console.log('Server running at port '+port)
 })
 
-const nodemailer = require('nodemailer')
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-        user: process.env.AUTH_EMAIL,
-        pass: process.env.AUTH_PASSWORD
-    }
-})
-
-transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready for messages");
-      console.log(success);
-    }
-  });
-
-function sendmail(){
-    const mailConfigurations = {
-        from: process.env.AUTH_EMAIL,
-        to: 'gaggup437@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'Hi! There, You know I am using the NodeJS '
-         + 'Code along with NodeMailer to send this email.'
-    };
-        
-    transporter.sendMail(mailConfigurations, function(error, info){
-        if (error) throw Error(error);
-           console.log('Email Sent Successfully');
-        console.log(info);
-    });
-}
-
-sendmail();
