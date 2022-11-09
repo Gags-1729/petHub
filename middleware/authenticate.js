5a95294bdd44409046966bd9666d007a1c228218
Gagan <gagan123.sp@gmail.com>
2022-11-09 14:35:33 +0530

    auth-token

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = function (req, res, next){
-    const token = req.header('auth_token')
+    const token = req.header('auth-token')
     if(!token){
         return res.status(400).json({
             status: 400,
