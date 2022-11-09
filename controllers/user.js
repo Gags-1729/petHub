73207ed9e9cab405004f260731ade07ef86d815f
Gagan <gagan123.sp@gmail.com>
2022-11-09 09:41:53 +0530

    alal

--- controllers/user.js
+++ controllers/user.js
@@ -33,7 +33,7 @@ exports.user_register = async(req, res, next) => {
 
 exports.user_find_all = async(req, res, next) => {
     try {
-        sendmail();
+        // sendmail();
         const users = await User.find();
         res.send(users);
     } catch(err){
@@ -57,37 +57,37 @@ exports.user_find_one = async(req, res, next) => {
     }
 }
 
-const nodemailer = require('nodemailer')
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-        user: process.env.AUTH_EMAIL,
-        pass: process.env.AUTH_PASSWORD
-    }
-})
+// const nodemailer = require('nodemailer')
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     host: "smtp.gmail.com",
+//     auth: {
+//         user: process.env.AUTH_EMAIL,
+//         pass: process.env.AUTH_PASSWORD
+//     }
+// })
 
-transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready for messages");
-      console.log(success);
-    }
-  });
+// transporter.verify((error, success) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Ready for messages");
+//       console.log(success);
+//     }
+//   });
 
-function sendmail(){
-    const mailConfigurations = {
-        from: process.env.AUTH_EMAIL,
-        to: 'gaggup437@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'Hi! There, You know I am using the NodeJS '
-         + 'Code along with NodeMailer to send this email.'
-    };
+// function sendmail(){
+//     const mailConfigurations = {
+//         from: process.env.AUTH_EMAIL,
+//         to: 'gaggup437@gmail.com',
+//         subject: 'Sending Email using Node.js',
+//         text: 'Hi! There, You know I am using the NodeJS '
+//          + 'Code along with NodeMailer to send this email.'
+//     };
         
-    transporter.sendMail(mailConfigurations, function(error, info){
-        if (error) throw Error(error);
-           console.log('Email Sent Successfully');
-        console.log(info);
-    });
-}
+//     transporter.sendMail(mailConfigurations, function(error, info){
+//         if (error) throw Error(error);
+//            console.log('Email Sent Successfully');
+//         console.log(info);
+//     });
+// }
